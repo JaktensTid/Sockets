2cf0544b9f4d31870e06af3b8b2e7417676987b4
JaktensTid <jaktenstid1@gmail.com>
2016-12-11 13:21:08 +0300

    Windows

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import logging
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit
 
 app = None
 BUTTON_ZERO = '0'
@@ -9,65 +9,81 @@ BUTTON_ONE = '1'
 BUTTON_TWO = '2'
 BUTTON_THREE = '3'
 
+
 class Client():
-	def __init__(self, address, port):
-		self.address = address
-		self.port = port
-		self.s = None
+    def __init__(self):
+        self.s = None
+        self.running = False
 
-	def start(self):
-		self.s = socket.socket()
-		self.s.connect((self.address, self.port))
+    def start(self, address):
+        if not self.running:
+            self.s = socket.socket()
+            self.s.connect((address, 9090))
+            self.running = True
 
-	def press_button(self, btn):
-		print(btn)
-		self.s.send(btn.encode())
+    def press_button(self, btn):
+        self.s.send(btn.encode())
 
-	def close(self):
-		self.s.close()
-
-	#Implemening disposable object to use with 'with'
-	def __enter__(self):
-		self.start()
-		return self
-
-	def __exit__(self, exc_type, exc_value, traceback):
-		if exc_type is not None:
-			logging.error('Type: ' + str(exc_type) + 
-				'. Value: ' + str(exc_value) + '. Traceback: ' + str(traceback))
-		self.close()
+    def close(self):
+        self.running = False
+        self.s.close()
 
 
 class Window():
-	def _place_button(text, x,y, widget, press_event):
-		button = QPushButton(text, widget)
-		button.resize(60,40)
-		button.move(x,y)
-		button.clicked.connect(press_event)
+    def _place_button(self, text, x, y, widget, flag):
+        button = QPushButton(text, widget)
+        button.resize(60, 40)
+        button.move(x, y)
+        button.clicked.connect(lambda: self.server.press_button(flag))
 
-	def create(self, handler):
-		def press_button(flag):
-			handler.press_button(flag)
+    def create(self, server):
+        def press_button(flag):
+            server.press_button(flag)
 
-		app = QApplication(sys.argv)
-		w = QWidget()
-		w.resize(180, 110)
-		self._place_button('Button 1', 20, 10, w, lambda: handler.press_button(BUTTON_ZERO))
-		self._place_button('Button 2', 90, 10, w, lambda: handler.press_button(BUTTON_ONE))
-		self._place_button('Button 3', 20, 60, w, lambda: handler.press_button(BUTTON_TWO))
-		self._place_button('Button 4', 90, 60, w, lambda: handler.press_button(BUTTON_THREE))
-		w.setWindowTitle('Client')
-		w.show()
-		try:
-			sys.exit(app.exec_())
-		except KeyboardInterrupt:
-			#Exit application
-			pass
+        self.server = server
+        app = QApplication(sys.argv)
+        w = QWidget()
+        w.resize(180, 160)
+        self._place_button('Button 1', 20, 60, w, BUTTON_ZERO)
+        self._place_button('Button 2', 90, 60, w, BUTTON_ONE)
+        self._place_button('Button 3', 20, 110, w, BUTTON_TWO)
+        self._place_button('Button 4', 90, 110, w, BUTTON_THREE)
+        self.textbox = QLineEdit(w)
+        self.textbox.move(10, 10)
+        self.textbox.resize(120, 40)  # 160
+        change_address_btn = QPushButton('OK', w)
+        change_address_btn.resize(30, 40)
+        change_address_btn.move(140, 10)
+        change_address_btn.clicked.connect(self.change_ip())
+        w.setWindowTitle('Client')
+        w.show()
+        try:
+            sys.exit(app.exec_())
+        except KeyboardInterrupt:
+            # Exit application
+            pass
+
+    def change_ip(self):
+        def click():
+            textbox_value = self.textbox.text().replace(' connected', '')
+            self.textbox.setText('Wait . . .')
+            if self.server.running:
+                self.server.close()
+            try:
+                self.server.start(textbox_value)
+                self.textbox.setText(textbox_value + ' connected')
+            except socket.gaierror:
+                self.textbox.setText('Invalid address')
+            except ConnectionRefusedError:
+                self.textbox.setText('Connection refused')
+
+        return click
+
 
 def main():
-	with Client('localhost',9090) as client:
-		window = Window()
-		window.create(client)
+    window = Window()
+    window.create(Client())
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- server.py
+++ server.py
@@ -1,52 +1,123 @@
 import socket
-import logging
+import signal
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+import threading
+from PyQt5.QtWidgets import QApplication, QWidget, QFrame, QLineEdit, QPushButton
 
 BUTTON_ZERO = '0'
 BUTTON_ONE = '1'
 BUTTON_TWO = '2'
 BUTTON_THREE = '3'
 
+server_thread = None
+
+
+def signal_handler(signal, frame):
+    print('Signal')
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+
+
 class Server():
-	def __init__(self, port, handler=None):
-		self.handler = handler
-		self.port = port
-		self.s = None
+    def __init__(self, handler):
+        self.s = None
+        self.handler = handler
+        self.running = False
 
-	def start(self):
-		self.s = socket.socket()
-		self.s.bind(('0.0.0.0',9090))
+    def start(self):
+        self.s = socket.socket()
+        self.s.bind(('0.0.0.0', 9090))
+        self._listen()
 
-	def listen(self):
-		self.s.listen(10)
-		connection, address = self.s.accept()
-		while True:
-			data = connection.recv(1).decode('utf-8')
-			if not data:
-				break
-			print(data)
-			#self.handler.get(data)
+    def _listen(self):
+        self.s.listen(10)
+        print('Listening')
+        self.running = True
+        connection, address = self.s.accept()
+        self.handler.connected(address)
+        while True:
+            try:
+                data = connection.recv(1).decode('utf-8')
+                if not data:
+                    break
+                print(data)
+                self.handler.get(data)
+            except ConnectionResetError:
+                print('Connection closed')
+                self.handler.reset()
+                self._listen()
 
-	def close(self):
-		self.s.close()
+    def close(self):
+        self.s.close()
 
-	#Implemening disposable object to use with 'with'
-	def __enter__(self):
-		self.start()
-		return self
 
-	def __exit__(self, exc_type, exc_value, traceback):
-		if exc_type is not None:
-			print('Type: ' + str(exc_type) + 
-				'. Value: ' + str(exc_value) + '. Traceback: ' + str(traceback))
-		self.close()
-		
+class Window():
+    def set_white(self, square):
+        square.setStyleSheet('QWidget { background-color: #FFFFFF}')
+
+    def set_black(self, square):
+        square.setStyleSheet('QWidget { background-color: #000000}')
+
+    def reset_color(self, square):
+        square.setStyleSheet('QWidget { background-color: #F0FFF0}')
+
+    def create(self, thread):
+        app = QApplication(sys.argv)
+        w = QWidget()
+        self.textbox = QLineEdit(w)
+        self.textbox.move(10, 10)
+        self.textbox.resize(120, 40)
+        self.textbox.setText('Not connected')
+        self.textbox.setReadOnly(True)
+        self.square1 = QFrame(w)
+        self.square1.setGeometry(10, 60, 192, 192)
+        self.square2 = QFrame(w)
+        self.square2.setGeometry(212, 60, 192, 192)
+        self.reset_color(self.square1)
+        self.reset_color(self.square2)
+        w.resize(414, 262)
+        w.setWindowTitle('Server')
+        print('Showing')
+        w.show()
+        try:
+            sys.exit(app.exec_())
+        except KeyboardInterrupt:
+            thread.exit()
+
+    def connected(self, address):
+        self.textbox.setText('Connected: ' + address[0])
+
+    def get(self, data):
+        if data == BUTTON_ZERO:
+            self.set_black(self.square1)
+            self.set_black(self.square2)
+        if data == BUTTON_ONE:
+            self.set_black(self.square1)
+            self.set_white(self.square2)
+        if data == BUTTON_TWO:
+            self.set_white(self.square1)
+            self.set_black(self.square2)
+        if data == BUTTON_THREE:
+            self.set_white(self.square1)
+            self.set_white(self.square2)
+
+    def reset(self):
+        self.reset_color(self.square1)
+        self.reset_color(self.square2)
+        self.textbox.setText('Not connected')
+
 
 def main():
-	with Server(9090) as server:
-		server.listen()
+    window = Window()
+    server = Server(window)
+    server_thread = threading.Thread(target=server.start)
+    server_thread.start()
+    window.create(server_thread)
+    server_thread.join()
 
 
 if __name__ == "__main__":
-	main()
+    main()
